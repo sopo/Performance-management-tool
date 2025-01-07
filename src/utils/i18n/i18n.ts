e02fe99db6043242171b28be8f737f32fea88daf
sopo <sofiko.imnaishvili@gmail.com>
2025-01-07 15:23:23 +0400

    add fallback language

--- src/utils/i18n/i18n.ts
+++ src/utils/i18n/i18n.ts
@@ -31,6 +31,8 @@ i18n
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
     // fallbackLng: "en",
+    lng: 'en', 
+    fallbackLng: 'en',
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
