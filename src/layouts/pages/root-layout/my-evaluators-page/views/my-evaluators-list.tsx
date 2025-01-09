abc17745c87d726b07a70bd89b3681e5dd6e7bfb
sopo <sofiko.imnaishvili@gmail.com>
2025-01-09 09:18:14 +0400

    add button link

--- src/layouts/pages/root-layout/my-evaluators-page/views/my-evaluators-list.tsx
+++ src/layouts/pages/root-layout/my-evaluators-page/views/my-evaluators-list.tsx
@@ -2,18 +2,24 @@ import { Peers } from "@/types/types"
 import MyEvaluatorsListItem from "../components/my-evaluators-list-item"
 import { Button } from "@/components/ui/button"
 import { useTranslation } from "react-i18next"
+import { useNavigate } from "react-router"
+import { ROOT_PATHS } from "../../root.enums"
 interface MyEvaluatorsListProps{
     users: Peers[]
 }
 const MyEvaluatorsList:React.FC<MyEvaluatorsListProps> = ({users}) => {
     const {t} = useTranslation()
+    const navigate = useNavigate()
+    const onClick = () => {
+        navigate(ROOT_PATHS.CHOOSE_EMPLOYEES)
+    }
     return(
         <div className="w-full flex flex-col gap-8">
             {users.map((user) => (
                 <MyEvaluatorsListItem user={user} key={user.peer_id}/>
               
             ))}
-            <Button>{t("pages.myEvaluators.addMorePeers")}</Button>
+            <Button onClick={onClick}>{t("pages.myEvaluators.addMorePeers")}</Button>
         </div>
     )
 }
