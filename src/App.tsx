510b372199ecf000568f3954e04dae448d49e574
sopo <sofiko.imnaishvili@gmail.com>
2025-01-10 14:45:26 +0400

    add light as default theme

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function App() {
   }
 
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <RouterProvider router={router} />
     </ThemeProvider>
   );
