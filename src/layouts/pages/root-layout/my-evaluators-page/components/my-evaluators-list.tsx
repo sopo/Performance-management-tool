14137c1fcd6f2e22640fa50b5cc2e06498b6f261
sopo <sofiko.imnaishvili@gmail.com>
2025-01-10 13:46:07 +0400

    remove screen

--- src/layouts/pages/root-layout/my-evaluators-page/components/my-evaluators-list.tsx
+++ src/layouts/pages/root-layout/my-evaluators-page/components/my-evaluators-list.tsx
@@ -1,9 +1,11 @@
 import { Peer } from "@/types/types"
-import MyEvaluatorsListItem from "../components/my-evaluators-list-item"
+import MyEvaluatorsListItem from "./my-evaluators-list-item"
 import { Button } from "@/components/ui/button"
 import { useTranslation } from "react-i18next"
 import { useNavigate } from "react-router"
 import { ROOT_PATHS } from "../../root.enums"
+
+import Text from "@/components/text/text"
 interface MyEvaluatorsListProps{
     users: Peer[]
 }
@@ -14,14 +16,15 @@ const MyEvaluatorsList:React.FC<MyEvaluatorsListProps> = ({users}) => {
         navigate(ROOT_PATHS.CHOOSE_EMPLOYEES)
     }
     return(
-        <div className="w-full flex flex-col gap-8">
-            <div>
-            {users.map((user) => (
+        <div className="flex flex-col gap-8">
+        <Text type="title-large">{t("pages.myEvaluators.title")}</Text>
+        <div className="w-full flex flex-col">
+        {users.map((user) => (
                 <MyEvaluatorsListItem peer={user} key={user.peer_id}/>
             ))}
-            </div>
-            <Button onClick={onClick}>{t("pages.myEvaluators.addMorePeers")}</Button>
         </div>
+        <Button onClick={onClick}>{t("pages.myEvaluators.addMorePeers")}</Button>
+      </div>  
     )
 }
 export default MyEvaluatorsList
--- src/layouts/pages/root-layout/my-evaluators-page/views/my-evaluators-page.tsx
+++ src/layouts/pages/root-layout/my-evaluators-page/views/my-evaluators-page.tsx
@@ -1,28 +1,21 @@
-import Screen from "@/components/containers/screen";
-import Text from "@/components/text/text";
-import { useTranslation } from "react-i18next";
 import useGetMyPeers from "@/hooks/use-get-my-peers";
 import { useAtomValue } from "jotai";
 import { UserAtom } from "@/store/auth";
-import MyEvaluatorsList from "./my-evaluators-list";
+import MyEvaluatorsList from "../components/my-evaluators-list";
 import EmptyState from "../components/my-evaluators-empty-state";
 
 const MyEvaluatorsPage: React.FC = () => {
-  const {t} = useTranslation()
-  const user = useAtomValue(UserAtom)
-  const userId = user?.user.id || ""
-  const {data} = useGetMyPeers({id: userId})
+  const user = useAtomValue(UserAtom);
+  const userId = user?.user.id || "";
+  const { data: users } = useGetMyPeers({ id: userId });
 
+  if (!users || users.length === 0) {
+    return (
+        <EmptyState />
+    );
+  }
   return (
-    <Screen>
-      <div className="flex flex-col gap-8">
-        <Text type="title-large">{t("pages.myEvaluators.title")}</Text>
-        <div className="flex flex-col gap-8 lg:flex-row lg:gap-8">
-    {data && data?.length > 0 ? <MyEvaluatorsList users={data} /> : <EmptyState />}
-         
-        </div>
-      </div>
-    </Screen>
+     <MyEvaluatorsList users={users} />
   );
 };
 export default MyEvaluatorsPage;
