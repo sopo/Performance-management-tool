f1327a0bed37e6efe6a3570226940cf9b4b6c739
sopo <sofiko.imnaishvili@gmail.com>
2025-01-12 14:42:21 +0400

    update database types

--- src/supabase/database.types.ts
+++ src/supabase/database.types.ts
@@ -256,12 +256,18 @@ export type TablesUpdate<
       : never
     : never
 
-export type Enums<
-  PublicEnumNameOrOptions extends
-    | keyof PublicSchema["Enums"]
-    | { schema: keyof Database },
-  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never,
-> = PublicEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+    export type Enums<
+    PublicEnumNameOrOptions extends
+      | keyof PublicSchema["Enums"]
+      | { schema: keyof Database },
+    EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+      ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+      : never = never
+  > = PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? EnumName extends keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+      ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+      : never
+    : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never;
+  
