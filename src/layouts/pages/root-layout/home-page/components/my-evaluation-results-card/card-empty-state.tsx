04f9bbe27c16330779f8495704f2c8d2e4227eb8
sopo <sofiko.imnaishvili@gmail.com>
2025-01-13 15:55:02 +0400

    add reports card

---
+++ src/layouts/pages/root-layout/home-page/components/my-evaluation-results-card/card-empty-state.tsx
@@ -0,0 +1,17 @@
+import { Info } from "lucide-react"
+import { useTranslation } from "react-i18next"
+
+const CardEmptyState:React.FC =() => {
+    const {t} = useTranslation()
+    return(
+        <>
+          <p  className="border-t border-border pt-4 text-foreground text-md">{t("pages.evaluateEmployees.emptyState.title")}</p>
+          <div className="flex gap-2 items-center border-b border-border pb-4 ">
+
+          <Info className="text-muted-foreground w-5 h-5"/>
+          <p className="text-muted-foreground text-sm">{t("pages.evaluateEmployees.emptyState.description")}</p>
+          </div>
+        </>
+    )
+}
+export default CardEmptyState
--- src/layouts/pages/root-layout/home-page/components/my-evaluation-results-card/my-evaluation-results-card.tsx
+++ src/layouts/pages/root-layout/home-page/components/my-evaluation-results-card/my-evaluation-results-card.tsx
@@ -1,20 +1,20 @@
 import { Button } from "@/components/ui/button"
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card"
 import { t } from "i18next"
-import { ArrowRight, BookOpenText } from "lucide-react"
+import { ArrowRight, ChartPie } from "lucide-react"
 import { useNavigate, useParams } from "react-router"
 import { ROOT_PATHS } from "../../../root.enums"
 import { UserAtom } from "@/store/auth"
 import { useAtomValue } from "jotai"
-import CardEmptyState from "./card-empty-state"
 import useGetPeersToEvaluate from "@/hooks/use-get-peers-to-evaluate"
 import FilledCardContent from "./filled-card-content"
+import CardEmptyState from "./card-empty-state"
 
-const EvaluatePeersCard:React.FC = () => {
+const MyEvaluationResultsCard:React.FC = () => {
   const navigate=useNavigate();
   const {lang} = useParams()
   const onClick =() => {
-    navigate(`/${lang}/${ROOT_PATHS.EVALUATE_EMPLOYEES}`)
+    navigate(`/${lang}/${ROOT_PATHS.REPORTS}`)
   }
   const user = useAtomValue(UserAtom)
   const userId = user?.user.id || ""
@@ -24,18 +24,18 @@ const EvaluatePeersCard:React.FC = () => {
         <Card className="flex-1">
         <CardHeader>
           <div className="flex justify-between items-center">
-          <CardTitle className="text-lg font-normal">{t("pages.evaluateEmployees.title")}</CardTitle>
-          <BookOpenText className="text-muted-foreground w-5 h-5" />
+          <CardTitle className="text-lg font-normal">{t("pages.reports.title")}</CardTitle>
+          <ChartPie className="text-muted-foreground w-5 h-5" />
           </div>
-          <CardDescription>{t("global.chooseEvaluators")}</CardDescription>
+          <CardDescription>{t("pages.reports.description")}</CardDescription>
         </CardHeader>
         <CardContent className="flex flex-col gap-2 ">
-          {data && data.length > 0 ?  <FilledCardContent peerNumber={data.length} /> : <CardEmptyState />}  
+          {data && data.length > 0 ?  <FilledCardContent /> : <CardEmptyState />}  
         </CardContent>
         <CardFooter className="w-full">
-          <Button onClick={onClick} className="w-full"><ArrowRight />{data && data.length > 0 ? t("global.evaluateEmployees") :t("global.evaluateEmployees")}</Button>
+          <Button onClick={onClick} className="w-full"><ArrowRight />{data && data.length > 0 ? t("pages.reports.title") :t("global.evaluateEmployees")}</Button>
         </CardFooter>
       </Card>
     )
 }
-export default EvaluatePeersCard
+export default MyEvaluationResultsCard
