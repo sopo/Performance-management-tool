3bc39bfa12e4ea97cbeb3a2f208644b3f4427273
sopo <sofiko.imnaishvili@gmail.com>
2025-01-13 16:05:16 +0400

    add component

--- src/layouts/pages/root-layout/evaluate-employees-page/evaluate-employees-page.tsx
+++ src/layouts/pages/root-layout/evaluate-employees-page/evaluate-employees-page.tsx
@@ -1,22 +1,45 @@
-
 import { useAtomValue } from "jotai";
 import { UserAtom } from "@/store/auth";
 import useGetPeersToEvaluate from "@/hooks/use-get-peers-to-evaluate";
 import EmptyState from "./components/evaluate-employees-empty-state";
-
 import EvaluateEmployeesList from "./components/evaluate-employees-list";
-const EvaluateEmployeesPage: React.FC = () => {
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
-    const user = useAtomValue(UserAtom)
-  const userId = user?.user.id || ""
-  const {data} = useGetPeersToEvaluate({id: userId})
-  console.log("ecal data", data)
-  if(!data || data.length === 0){
-    return <EmptyState />
+const EvaluateEmployeesPage: React.FC = () => {
+  const user = useAtomValue(UserAtom);
+  const userId = user?.user.id || "";
+  const { data } = useGetPeersToEvaluate({ id: userId });
+  console.log("ecal data", data);
+  if (!data || data.length === 0) {
+    return <EmptyState />;
   }
 
   return (
-    <EvaluateEmployeesList peers={data} />
+    <>
+      <EvaluateEmployeesList peers={data} />
+
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href="#" />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </>
   );
 };
 export default EvaluateEmployeesPage;
