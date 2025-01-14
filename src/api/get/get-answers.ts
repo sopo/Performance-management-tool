0044ad73a404cdf24ec9663917fb46e73b8f0e3a
sopo <sofiko.imnaishvili@gmail.com>
2025-01-14 10:34:52 +0400

    add get questions and answers

--- src/api/get/get-answers.ts
+++ src/api/get/get-answers.ts
@@ -1,6 +1,6 @@
 import { supabase } from "@/supabase";
 
 export const getAnswers = async (id: string) => {
-  const { data } = await supabase.from("answers").select("*").eq("user_id", id);
+  const { data } = await supabase.from("answers").select("*").eq("peer_id", id);
   return data;
 };
--- src/layouts/pages/root-layout/reports-page/reports-page.tsx
+++ src/layouts/pages/root-layout/reports-page/reports-page.tsx
@@ -1,11 +1,29 @@
 import Text from "@/components/text/text";
+import useGetAnswers from "@/hooks/use-get-answers";
+import useGetQuestions from "@/hooks/use-get-questions";
+import { UserAtom } from "@/store/auth";
+import { useAtomValue } from "jotai";
 import { useTranslation } from "react-i18next";
 const ReportsPage: React.FC = () => {
   const { t } = useTranslation();
+  const user = useAtomValue(UserAtom);
+  const userId = user?.user.id || "";
+  const {data} = useGetAnswers({id: userId})
+  const {data: questions} = useGetQuestions()
+  console.log("ans", data)
+  console.log("userId", userId)
   return (
     <div className="flex flex-col gap-8">
       <Text type="title-large">{t("pages.reports.title")}</Text>
-      <div className="flex flex-col gap-8 lg:flex-row lg:gap-8"></div>
+
+      <div className="flex flex-col gap-8 lg:flex-row lg:gap-8">
+        {data?.map((answer) => {
+        return <p>{answer.question_id} - score {answer.score}</p> 
+        })}
+        {questions?.map((question) => {
+          return <p>{question.id} - {question.title_en}</p>
+})}
+      </div>
     </div>
   );
 };
