13e178d7e11c3b0b75c0840a8d6fffab32913c1b
sopo <sofiko.imnaishvili@gmail.com>
2025-01-14 17:01:12 +0400

    add empty state messages

--- src/layouts/pages/root-layout/home-page/components/choose-peers-card/card-empty-state.tsx
+++ src/layouts/pages/root-layout/home-page/components/choose-peers-card/card-empty-state.tsx
@@ -5,11 +5,11 @@ const CardEmptyState:React.FC =() => {
     const {t} = useTranslation()
     return(
         <>
-          <p  className="border-t border-border pt-4 text-foreground text-md">{t("pages.evaluateEmployees.emptyState.title")}</p>
+          <p  className="border-t border-border pt-4 text-foreground text-md">{t("pages.chooseEvaluators.emptyState.title")}</p>
           <div className="flex gap-2 items-center border-b border-border pb-4 ">
 
           <Info className="text-muted-foreground w-5 h-5"/>
-          <p className="text-muted-foreground text-sm">{t("pages.evaluateEmployees.emptyState.description")}</p>
+          <p className="text-muted-foreground text-sm">{t("pages.chooseEvaluators.emptyState.description")}</p>
           </div>
         </>
     )
