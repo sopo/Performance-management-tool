c49ca0b6a2b834f54b4258bc71cdb059b9f6253b
sopo <sofiko.imnaishvili@gmail.com>
2025-01-14 11:28:11 +0400

    add report title

--- src/layouts/pages/root-layout/reports-page/reports-page.tsx
+++ src/layouts/pages/root-layout/reports-page/reports-page.tsx
@@ -4,25 +4,59 @@ import useGetQuestions from "@/hooks/use-get-questions";
 import { UserAtom } from "@/store/auth";
 import { useAtomValue } from "jotai";
 import { useTranslation } from "react-i18next";
+import { useParams } from "react-router";
 const ReportsPage: React.FC = () => {
   const { t } = useTranslation();
+  const {lang} = useParams()
   const user = useAtomValue(UserAtom);
   const userId = user?.user.id || "";
-  const {data} = useGetAnswers({id: userId})
-  const {data: questions} = useGetQuestions()
-  console.log("ans", data)
-  console.log("userId", userId)
+  const { data: answers } = useGetAnswers({ id: userId });
+  const { data: questions } = useGetQuestions();
+  const sumOfEvaluators = [...new Set(answers?.map((answer) => answer.user_id))].length;
+
   return (
     <div className="flex flex-col gap-8">
       <Text type="title-large">{t("pages.reports.title")}</Text>
-
-      <div className="flex flex-col gap-8 lg:flex-row lg:gap-8">
-        {data?.map((answer) => {
-        return <p>{answer.question_id} - score {answer.score}</p> 
-        })}
+      <div className="flex flex-col gap-8">
+        {answers && (
+          <Text type="title-medium">
+            {t("pages.reports.reportTitle")} {sumOfEvaluators}{" "}
+            {t("pages.reports.evaluators")}
+          </Text>
+        )}
         {questions?.map((question) => {
-          return <p>{question.id} - {question.title_en}</p>
-})}
+          const filteredAnswers = answers?.filter(
+            (answer) => answer.question_id === question.id
+          );
+
+          const averageScore = filteredAnswers
+            ? filteredAnswers.reduce(
+                (acc, answer) => acc + (answer.score || 0),
+                0
+              ) / filteredAnswers.length
+            : null;
+
+          return (
+            <div key={question.id} >
+              <p className="text-xl text-foreground">
+                {lang === "en" ? question.title_en : question.title_ka}
+              </p>
+              <div>
+                {filteredAnswers && (
+                  <>
+                    {filteredAnswers.map((answer) => (
+                      <p key={answer.id}>Answer: {answer.score}</p>
+                    ))}
+                    <p>
+                      Average Score:{" "}
+                      {averageScore && averageScore.toFixed(2) }
+                    </p>
+                  </>
+                )}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
