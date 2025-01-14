837ecd05a5191a0c453fe54e6b075bafb9d37349
sopo <sofiko.imnaishvili@gmail.com>
2025-01-14 13:56:53 +0400

    moved card to different component

---
+++ src/layouts/pages/root-layout/reports-page/components/results-by-questions-card.tsx
@@ -0,0 +1,50 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Answer, Questions } from "@/types/types";
+import { useTranslation } from "react-i18next";
+import { useParams } from "react-router";
+interface ResultsByQuestionsCardProps{
+    questions: Questions[],
+    answers: Answer[]
+}
+const ResultsByQuestionsCard:React.FC<ResultsByQuestionsCardProps> = ({questions, answers}) => {
+    const {lang} = useParams()
+    const {t} = useTranslation()
+
+    return(
+        <Card>
+        <CardHeader>
+          <CardTitle>
+            <p className="text-sm font-semibold uppercase text-muted-foreground">
+              {t("pages.reports.scoreByQuestions")}
+            </p>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          {questions?.map((question) => {
+            const filteredAnswers = answers?.filter(
+              (answer) => answer.question_id === question.id
+            );
+
+            const averageScore = filteredAnswers
+              ? filteredAnswers.reduce(
+                  (acc, answer) => acc + (answer.score || 0),
+                  0
+                ) / filteredAnswers.length
+              : null;
+
+            return (
+              <div key={question.id} className="mb-6">
+                <p className="text-xl font-medium text-foreground mb-1">
+                  {lang === "en" ? question.title_en : question.title_ka}
+                </p>
+                <p>
+                  {`${t("pages.reports.score")} ${averageScore && averageScore.toFixed(2)}/10`}
+                </p>
+              </div>
+            );
+          })}
+        </CardContent>
+      </Card>
+    )
+}
+export default ResultsByQuestionsCard
--- src/layouts/pages/root-layout/reports-page/reports-page.tsx
+++ src/layouts/pages/root-layout/reports-page/reports-page.tsx
@@ -1,16 +1,14 @@
 import Text from "@/components/text/text";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import useGetAnswers from "@/hooks/use-get-answers";
 import useGetQuestions from "@/hooks/use-get-questions";
 import { UserAtom } from "@/store/auth";
 import { useAtomValue } from "jotai";
-import { SquareChartGantt } from "lucide-react";
 import { useTranslation } from "react-i18next";
-import { useParams } from "react-router";
+import ResultsByQuestionsCard from "./components/results-by-questions-card";
+import { Answer, Questions } from "@/types/types";
 
 const ReportsPage: React.FC = () => {
   const { t } = useTranslation();
-  const { lang } = useParams();
   const user = useAtomValue(UserAtom);
   const userId = user?.user.id || "";
   const { data: answers } = useGetAnswers({ id: userId });
@@ -29,40 +27,8 @@ const ReportsPage: React.FC = () => {
           </p>
         )}
       </div>
-      <Card>
-        <CardHeader>
-          <CardTitle>
-            <p className="text-sm font-semibold uppercase text-muted-foreground">
-              {t("pages.reports.scoreByQuestions")}
-            </p>
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          {questions?.map((question) => {
-            const filteredAnswers = answers?.filter(
-              (answer) => answer.question_id === question.id
-            );
-
-            const averageScore = filteredAnswers
-              ? filteredAnswers.reduce(
-                  (acc, answer) => acc + (answer.score || 0),
-                  0
-                ) / filteredAnswers.length
-              : null;
-
-            return (
-              <div key={question.id} className="mb-6">
-                <p className="text-xl font-medium text-foreground mb-1">
-                  {lang === "en" ? question.title_en : question.title_ka}
-                </p>
-                <p>
-                  {`${t("pages.reports.score")} ${averageScore && averageScore.toFixed(2)}/10`}
-                </p>
-              </div>
-            );
-          })}
-        </CardContent>
-      </Card>
+     
+      <ResultsByQuestionsCard questions={questions as Questions[]} answers={answers as Answer[]} />
     </div>
   );
 };
