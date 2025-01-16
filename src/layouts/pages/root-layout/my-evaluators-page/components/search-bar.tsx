84a25866c834be460703b77f149cec003663a69f
sopo <sofiko.imnaishvili@gmail.com>
2025-01-16 12:11:00 +0400

    add search to choose employees

--- src/layouts/pages/root-layout/my-evaluators-page/components/search-bar.tsx
+++ src/layouts/pages/root-layout/my-evaluators-page/components/search-bar.tsx
@@ -1,17 +1,49 @@
+import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
+import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
+import { useSearchParams } from "react-router";
+import { useDebounce } from "@/hooks/use-debounce";
+import { useEffect } from "react";
+import qs from "qs";
+
 
 const SearchBar: React.FC = () => {
   const { t } = useTranslation();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const parsedQueryParams = qs.parse(searchParams.toString());
+  const searchQuery =
+    typeof parsedQueryParams.search === "string"
+      ? parsedQueryParams.search
+      : "";
+
+  const { register, watch } = useForm({
+    defaultValues: {
+      search: searchQuery,
+    },
+  });
+  const search = register("search");
+  const enteredText = watch("search");
+  const debouncedSearch = useDebounce(enteredText);
+
+  useEffect(() => {
+    if (debouncedSearch.length > 0) {
+      const newQueryParams = { search: debouncedSearch };
+      const serializedParams = qs.stringify(newQueryParams, {
+        skipNulls: true,
+      });
+      setSearchParams(serializedParams);
+    } else {
+      setSearchParams({});
+    }
+  }, [debouncedSearch, setSearchParams]);
   return (
-    <label className="bg-gray-50 flex items-center h-12 px-4 border border-border rounded-sm">
-      <Search className="w-6 h-6 text-muted-foreground" />
-      <input
-        type="search"
-        placeholder={t("global.search")}
-        className="w-full bg-transparent border-none focus:outline-none focus:border-primary"
-      />
-    </label>
+
+    <div className="relative">
+    <Input {...search} placeholder={t("global.search")} className="pl-8" />
+    <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground" />
+  </div>
   );
 };
 export default SearchBar;
