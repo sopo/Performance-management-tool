86b442c6fc9629ab634ce87036f64214fc24f425
sopo <sofiko.imnaishvili@gmail.com>
2025-01-21 12:58:23 +0400

    fix link in evaluate employees

--- src/layouts/pages/root-layout/evaluate-employees-page/components/evaluate-employees-list-item.tsx
+++ src/layouts/pages/root-layout/evaluate-employees-page/components/evaluate-employees-list-item.tsx
@@ -1,6 +1,7 @@
 import { Peer } from "@/types/types";
 import { ArrowRight, CircleCheck } from "lucide-react";
 import PeerListItem from "./peer-list-item";
+import { useNavigate } from "react-router";
 
 interface EvaluateEmployeesListItemProps {
   peer: Peer;
@@ -8,10 +9,12 @@ interface EvaluateEmployeesListItemProps {
 const EvaluateEmployeesListItem: React.FC<EvaluateEmployeesListItemProps> = ({
   peer,
 }) => {
+  const navigate = useNavigate()
   return (
     <div
       key={peer.id}
       className="border-b border-border py-6 flex items-center justify-between hover:cursor-pointer"
+      onClick={() => navigate(`${peer.user_id}`)}
     >
       <PeerListItem peer={peer} />
       <p>
