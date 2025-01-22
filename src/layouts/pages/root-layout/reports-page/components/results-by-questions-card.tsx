9a582fd7f01897ae76178450e529b702433c0ce2
sopo <sofiko.imnaishvili@gmail.com>
2025-01-22 10:23:59 +0400

    add pie chart

--- src/layouts/pages/root-layout/reports-page/components/results-by-questions-card.tsx
+++ src/layouts/pages/root-layout/reports-page/components/results-by-questions-card.tsx
@@ -1,7 +1,8 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Answer, Questions } from "@/types/types";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router";
+
 interface ResultsByQuestionsCardProps {
   questions: Questions[];
   answers: Answer[];
@@ -12,8 +13,9 @@ const ResultsByQuestionsCard: React.FC<ResultsByQuestionsCardProps> = ({
 }) => {
   const { lang } = useParams();
   const { t } = useTranslation();
-
+ 
   return (
+    <>
     <Card>
       <CardHeader>
         <CardTitle>
@@ -36,6 +38,7 @@ const ResultsByQuestionsCard: React.FC<ResultsByQuestionsCardProps> = ({
             : null;
 
           return (
+        
             <div key={question.id} className="mb-6">
               <p className="text-xl font-medium text-foreground mb-1">
                 {lang === "en" ? question.title_en : question.title_ka}
@@ -48,6 +51,9 @@ const ResultsByQuestionsCard: React.FC<ResultsByQuestionsCardProps> = ({
         })}
       </CardContent>
     </Card>
+
+
+</>
   );
 };
 export default ResultsByQuestionsCard;
