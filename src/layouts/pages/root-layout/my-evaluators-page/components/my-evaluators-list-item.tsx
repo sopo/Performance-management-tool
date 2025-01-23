79996bf04dc0d8609b00206ff7906b9d9791c127
sopo <sofiko.imnaishvili@gmail.com>
2025-01-23 11:08:09 +0400

    prettier

--- src/layouts/pages/root-layout/my-evaluators-page/components/my-evaluators-list-item.tsx
+++ src/layouts/pages/root-layout/my-evaluators-page/components/my-evaluators-list-item.tsx
@@ -4,7 +4,13 @@ import { Trash2 } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 import PeerListItem from "./peer-list-item";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { useTranslation } from "react-i18next";
 import { useQueryClient } from "@tanstack/react-query";
@@ -19,26 +25,25 @@ const MyEvaluatorsListItem: React.FC<PeerListItemProps> = ({ peer }) => {
   const { lang } = useParams();
   const { t } = useTranslation();
   const queryClient = useQueryClient();
-  const [isDialogOpen, setIsDialogOpen] = useState(false)
-  const user = useAtomValue(UserAtom)
-  const userId =user?.user.id
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const user = useAtomValue(UserAtom);
+  const userId = user?.user.id;
   const navigate = useNavigate();
 
   const { mutate: deleteUser, isSuccess } = useDeleteUser();
 
-  
   const peerIdToDelete = peer?.id || 0;
   const handleDelete = (id: number | undefined) => {
     if (!id) {
       return;
     }
-      deleteUser(id);    
+    deleteUser(id);
   };
 
   useEffect(() => {
     if (isSuccess && userId) {
-      queryClient.invalidateQueries({queryKey: [QUERY_KEYS.PEERS, userId]});
-      setIsDialogOpen(false)
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.PEERS, userId] });
+      setIsDialogOpen(false);
     }
   }, [isSuccess, navigate, lang, queryClient, userId]);
 
@@ -47,20 +52,26 @@ const MyEvaluatorsListItem: React.FC<PeerListItemProps> = ({ peer }) => {
       <PeerListItem peer={peer} />
 
       <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-      <DialogTrigger asChild>
-        <Button variant="outline">
-          <Trash2 className="text-muted-foreground" />
-        </Button>
-      </DialogTrigger>
+        <DialogTrigger asChild>
+          <Button variant="outline">
+            <Trash2 className="text-muted-foreground" />
+          </Button>
+        </DialogTrigger>
 
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>{t("pages.myEvaluators.deleteEvaluator.title")}</DialogTitle>
-        </DialogHeader>
-        <p className="text-muted-foreground">{t("pages.myEvaluators.deleteEvaluator.description")}</p>
-        <Button onClick={() => handleDelete(peerIdToDelete)}>{t("pages.myEvaluators.deleteEvaluator.cta")}</Button>
-      </DialogContent>
-    </Dialog>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>
+              {t("pages.myEvaluators.deleteEvaluator.title")}
+            </DialogTitle>
+          </DialogHeader>
+          <p className="text-muted-foreground">
+            {t("pages.myEvaluators.deleteEvaluator.description")}
+          </p>
+          <Button onClick={() => handleDelete(peerIdToDelete)}>
+            {t("pages.myEvaluators.deleteEvaluator.cta")}
+          </Button>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
