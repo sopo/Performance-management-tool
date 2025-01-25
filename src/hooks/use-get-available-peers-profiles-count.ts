1bbda9b4204085deede463c9c90283ab606fb8ee
sopo <sofiko.imnaishvili@gmail.com>
2025-01-25 19:12:19 +0400

    fix pagination when search

--- src/hooks/use-get-available-peers-profiles-count.ts
+++ src/hooks/use-get-available-peers-profiles-count.ts
@@ -4,13 +4,15 @@ import { getAvailablePeersProfilesCount } from "@/api/get/get-profiles";
 
 const useGetAvailablePeersProfilesCount = ({
   id,
+  searchQuery
 }: {
   id: string;
+  searchQuery: string;
 }): UseQueryResult<number> => {
   return useQuery<number>({
-    queryKey: [QUERY_KEYS.PEERS_TO_EVALUATE_COUNT, id],
+    queryKey: [QUERY_KEYS.PEERS_TO_EVALUATE_COUNT, id, searchQuery],
     queryFn: async () => {
-      const result = await getAvailablePeersProfilesCount(id);
+      const result = await getAvailablePeersProfilesCount(id, searchQuery);
       return result || 0;
     },
   });
