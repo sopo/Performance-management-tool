b00e87d33c40732352d317917048dd8ea6cf2576
sopo <sofiko.imnaishvili@gmail.com>
2025-01-25 21:09:57 +0400

    add empty state for reports

--- src/layouts/pages/root-layout/reports-page/utils/get-average-score.ts
+++ src/layouts/pages/root-layout/reports-page/utils/get-average-score.ts
@@ -2,7 +2,7 @@ import { Answer } from "@/types/types";
 import mean from "lodash/mean";
 export const getAverageScore = (answers: Answer[]) => {
   if (!answers || answers === undefined) {
-    return;
+    return 0;
   }
   const validAnswers = answers.filter(
     (answer) => answer.score !== null && answer.score !== undefined,
@@ -11,5 +11,5 @@ export const getAverageScore = (answers: Answer[]) => {
 
   const average = mean(scores);
 
-  return average;
+  return average ?? 0;
 };
